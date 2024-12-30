c722429f813dec42c3acb42430f1136ee0978351
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2024-12-30 19:47:46 +0400

    feat: rendering categories table data from API

---
+++ src/app/dashboard/categories/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Table from 'rc-table';
+
+import { request } from '@/services/request';
+import './styles.scss';
+
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    key: 'id',
+    width: 100,
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    width: 100,
+  },
+  {
+    title: 'Operations',
+    dataIndex: '',
+    key: 'operations',
+    render: () => <a href='#'>Delete</a>,
+  },
+];
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = async () => {
+    const res = await request({ method: 'GET', url: '/categories' });
+    setCategories(res.data);
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  return (
+    <>
+      <h1>Categories page</h1>
+      <Table columns={columns} data={categories} rowKey={(record: any) => record.id} />
+    </>
+  );
+};
+
+export default Categories;

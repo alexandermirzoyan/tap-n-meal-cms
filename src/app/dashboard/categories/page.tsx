b85755a75a415fd1851b6f0e11ebb41eff73c4b1
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-01-02 14:20:44 +0400

    feat: remove category functionality developed

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,31 +1,12 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import Table from 'rc-table';
 
 import { request } from '@/services/request';
+import TrashIcon from '../../../../public/icons/trash.png';
 import './styles.scss';
-
-const columns = [
-  {
-    title: 'ID',
-    dataIndex: 'id',
-    key: 'id',
-    width: 100,
-  },
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: '',
-    dataIndex: '',
-    key: 'operations',
-    width: 100,
-    render: () => <a href='#'>Delete</a>,
-  },
-];
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
@@ -34,6 +15,43 @@ const Categories = () => {
     const res = await request({ method: 'GET', url: '/categories' });
     setCategories(res.data);
   };
+
+  const removeCategory = async (id: number) => {
+    const isRemoveConfirmed = window.confirm('Are you sure to remove ?');
+    if (!isRemoveConfirmed) {
+      return;
+    }
+
+    const res = await request({ method: 'DELETE', url: `/categories/${id}` });
+    if (res.data?.affected >= 1) {
+      getCategories();
+    }
+  };
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+      width: 100,
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '',
+      dataIndex: '',
+      key: 'operations',
+      width: 100,
+      render: (element: any) => (
+        <button className='rc-table-remove-col' onClick={() => removeCategory(element.id)}>
+          <Image alt='aa' src={TrashIcon} />
+        </button>
+      ),
+    },
+  ];
 
   useEffect(() => {
     getCategories();

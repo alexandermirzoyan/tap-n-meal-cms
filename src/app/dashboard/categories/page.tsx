732d10414e9616eb7b84d58ada29e34657d08a1e
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-01-03 13:25:27 +0400

    refactor(Categories page): using custom <Table /> component

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,12 +1,24 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import Image from 'next/image';
-import Table from 'rc-table';
 
 import { request } from '@/services/request';
-import TrashIcon from '../../../../public/icons/trash.png';
+import { Table } from '@/components/Table';
 import './styles.scss';
+
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    key: 'id',
+    width: 100,
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+];
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
@@ -28,39 +40,14 @@ const Categories = () => {
     }
   };
 
-  const columns = [
-    {
-      title: 'ID',
-      dataIndex: 'id',
-      key: 'id',
-      width: 100,
-    },
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '',
-      dataIndex: '',
-      key: 'operations',
-      width: 100,
-      render: (element: any) => (
-        <button className='rc-table-remove-col' onClick={() => removeCategory(element.id)}>
-          <Image alt='aa' src={TrashIcon} />
-        </button>
-      ),
-    },
-  ];
-
   useEffect(() => {
     getCategories();
   }, []);
 
   return (
     <>
-      <h1>Categories page</h1>
-      <Table columns={columns} data={categories} rowKey={(record: any) => record.id} />
+      <h1 className='mb-24'>Categories page</h1>
+      <Table columns={columns} data={categories} onRowRemove={removeCategory} />
     </>
   );
 };
